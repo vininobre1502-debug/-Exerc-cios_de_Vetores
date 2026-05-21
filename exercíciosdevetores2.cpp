b0eea9141e8c81b2458add1112a6275219b5382b
vininobre1502-debug <vininobre1502@gmail.com>
2026-05-21 09:30:49 -0300

    Update exercíciosdevetores2.cpp

--- exercíciosdevetores2.cpp
+++ exercíciosdevetores2.cpp
@@ -5,18 +5,15 @@ int main() {
     int v1[10], v2[10];
     int i;
 
-    // LOOP 1: Apenas para leitura dos dados
     for(i = 0; i < 10; i++) {
         printf("Digite o valor %d: ", i + 1);
         scanf("%d", &v1[i]);
     }
 
-    // LOOP 2: Separado apenas para inverter e passar para o v2
     for(i = 0; i < 10; i++) {
-        v2[i] = v1[9 - i]; // Pega do final de v1 e joga no inicio de v2
+        v2[i] = v1[9 - i];
     }
 
-    // Exibindo os resultados
     printf("\nVetor original (v1): ");
     for(i = 0; i < 10; i++) {
         printf("%d ", v1[i]);
